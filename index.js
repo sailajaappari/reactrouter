45376f36fa27e317da78729b6aef64fbdff94172
sailajaappari <sailajakadali@gmail.com>
2016-07-13 16:12:12 +0530

    2nd commit

--- index.js
+++ index.js
@@ -1,36 +1,75 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { Router, Route, hashHistory } from 'react-router'  
+import { Router, Route, hashHistory, IndexRoute } from 'react-router'  
+
+var MainLayout = React.createClass({
+  render: function () {
+    return (
+      <div className="app">
+        <header className="primary-header" />
+        <aside className="primary-aside" />
+        <main>
+          {this.props.children}
+        </main>
+      </div>
+    );
+  }
+});
 
 var Home = React.createClass({
-  render: function () {
+  render: function() {
     return (
-      <h1>Welcome to home page</h1>
+      <div><h1>Welcome to home page</h1></div>
     );
   }
 });
 
-var Users = React.createClass({
+var SearchLayout = React.createClass({
   render: function () {
     return (
-      <h2>Welcome to users page</h2>
+      <div className="search">
+        <header className="search-header" />
+        <div className="results"> 
+           {this.props.children}
+        </div>
+        
+        <div className="serach-footer" /> 
+      </div>
     );
   }
 });
 
-var Widgets = React.createClass({
+var UserList = React.createClass({
   render: function () {
     return (
-      <h2>Welcome to widgets page</h2>
+      <div>
+        <ul className="user-list">
+          <li>Dan</li>
+          <li>Ryan</li>
+          <li>Michael</li>
+        </ul>
+      </div>
+    );
+  }
+});
+
+var WidgetList = React.createClass({
+  render: function() {
+    return (
+      <div><h2>Welcome to widget list</h2></div>
     );
   }
 });
 
 render(
   <Router history={hashHistory}>
-    <Route path="/" component={Home} />
-    <Route path="/users" component={Users} />
-    <Route path="/widgets" component={Widgets} />
+    <Route path="/" component={MainLayout}>
+      <IndexRoute component={Home} />
+      <Route path="/search" component={SearchLayout}>
+        <Route path="/search/users" component={UserList} />
+        <Route path="/search/widget" component={WidgetList} />
+      </Route>
+    </Route>
   </Router>, 
   document.getElementById('app')
 );
